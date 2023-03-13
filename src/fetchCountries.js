a8df245025c8b480f36f3af8a5df3ba5293c3900
cmdspeed <cmdspeed@gmail.com>
2023-03-13 11:35:44 +0100

    Filtrowanie pól

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,8 @@
 const API = 'https://restcountries.com/v3.1/name';
+const FIELDS = 'name,capital,population,flags,languages';
 
 function fetchCountries(name) {
-    return fetch(`${API}/${name}?fullText=true`).then(response => {
+    return fetch(`${API}/${name}?fields=${FIELDS}`).then(response => {
         if (!response.ok) {
             throw new Error(response.status);
         }
