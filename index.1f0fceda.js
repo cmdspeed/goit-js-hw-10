83bcbe08860281469b38e204e59d97f9802b685a
cmdspeed <102898515+cmdspeed@users.noreply.github.com>
2023-03-13 10:36:07 +0000

    Deploying to gh-pages from @ cmdspeed/goit-js-hw-10@a8df245025c8b480f36f3af8a5df3ba5293c3900 🚀

--- index.1f0fceda.js
+++
@@ -1,2 +0,0 @@
-!function(){var t;document.querySelector("#search-box");console.log((t="poland",fetch("".concat("https://restcountries.com/v3.1/name","/").concat(t,"?fullText=true")).then((function(t){if(!t.ok)throw new Error(t.status);return t.json()}))))}();
-//# sourceMappingURL=index.1f0fceda.js.map
---
+++ index.796d1662.js
@@ -0,0 +1,2 @@
+!function(){var o;document.querySelector("#search-box");console.log((o="poland",fetch("".concat("https://restcountries.com/v3.1/name","/").concat(o,"?fields=").concat("name,capital,population,flags,languages")).then((function(o){if(!o.ok)throw new Error(o.status);return o.json()}))))}();
+//# sourceMappingURL=index.796d1662.js.map
