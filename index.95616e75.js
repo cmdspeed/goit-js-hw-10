44161a3af005547704824e2336341847d4692576
cmdspeed <102898515+cmdspeed@users.noreply.github.com>
2023-03-13 10:57:08 +0000

    Deploying to gh-pages from @ cmdspeed/goit-js-hw-10@b08f2cdb192916d9b7ad9bb458c4d10b2d9e1164 🚀

--- index.95616e75.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#search-box");console.log(fetch(`https://restcountries.com/v3.1/name/${"poland"}?fields=name,capital,population,flags,languages`).then((e=>{if(!e.ok)throw new Error(e.status);return e.json()})));
-//# sourceMappingURL=index.95616e75.js.map
