b08f2cdb192916d9b7ad9bb458c4d10b2d9e1164
cmdspeed <cmdspeed@gmail.com>
2023-03-13 11:56:45 +0100

    Pole wyszukiwania - console logi

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
-const DEBOUNCE_DELAY = 300;
-const searchInput = document.querySelector('#search-box');
+import debounce from 'lodash.debounce';
 
-console.log(fetchCountries(`poland`));
+const DEBOUNCE_DELAY = 300;
+const searchInput = document.getElementById('search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+const search = event => {
+    const value = event.target.value.trim();
+    if (value.length > 0) {
+        console.log(fetchCountries(value));
+    }
+};
+// console.log(fetchCountries(`poland`));
+searchInput.addEventListener('input', debounce(search, DEBOUNCE_DELAY));
